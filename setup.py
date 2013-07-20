3a7b3f5597b17824c2e5342ac22ec755df124f24
Jason Simeone <jay@classless.net>
2013-07-20 15:33:17 -0400

    Some tweaks to the setup script.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup, find_packages
+import os.path
+
+from setuptools import setup
 
  
 setup(
     name='datachecker',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['datachecker'],
     author='Clover Wireless',
     author_email='opensource@twigtek.com',
     license='MIT',
+    keywords=['datachecker', 'data', 'validation', 'validate', 'sanitization', 'sanitize'],
     description='A simple library for performing common validations and sanitization of data.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     url='https://bitbucket.org/cloverwireless/datachecker',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -16,6 +20,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
+    extras_require={
+        'DNS': ['dnspython>=1.0'],
+    },
 )
 
